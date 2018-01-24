3dd860df4e483d782b4c1af6364b6f4458dde71b
hussu97 <h_abbasi97@hotmail.com>
2018-01-24 11:12:33 +0400

    Completed application, added styling and jQuery functionality

--- assets/js/toDoList.js
+++ assets/js/toDoList.js
@@ -1,9 +1,20 @@
-$('li').on("click",function(){
+var toDoText;
+$('ul').on("click","li",function(){
   $(this).toggleClass("completedItem");
 })
-$('span').on("click",function(event){
+$('ul').on("click","span",function(event){
   $(this).parent().slideUp(600,function(){
     $(this).remove();
   });
   event.stopPropagation();
+})
+$("input[type='text']").keypress(function(event){
+  if(event.which===13){
+    toDoText=$(this).val();
+    $("ul").append("<li><span class='fa fa-trash'> </span>"+toDoText+"</li>");
+    $(this).val("");
+  }
+})
+$(".fa-pencil-square-o").on("click",function(){
+  $("input[type='text']").fadeToggle(1000);
 })
