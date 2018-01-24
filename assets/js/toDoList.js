ab773ea352f9ba2c3e9859c791ecaf4c7fcd9a91
hussu97 <h_abbasi97@hotmail.com>
2018-01-24 13:32:14 +0400

    Final version

--- assets/js/toDoList.js
+++ assets/js/toDoList.js
@@ -1,20 +1,20 @@
 var toDoText;
-$('ul').on("click","li",function(){
+$('ul').on("click", "li", function() {
   $(this).toggleClass("completedItem");
 })
-$('ul').on("click","span",function(event){
-  $(this).parent().slideUp(600,function(){
+$('ul').on("click", "span", function(event) {
+  $(this).parent().slideUp(600, function() {
     $(this).remove();
   });
   event.stopPropagation();
 })
-$("input[type='text']").keypress(function(event){
-  if(event.which===13){
-    toDoText=$(this).val();
-    $("ul").append("<li><span class='fa fa-trash'> </span>"+toDoText+"</li>");
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    toDoText = $(this).val();
+    $("ul").append("<li><span class='fa fa-trash'> </span>" + toDoText + "</li>");
     $(this).val("");
   }
 })
-$(".fa-pencil-square-o").on("click",function(){
+$(".fa-pencil-square-o").on("click", function() {
   $("input[type='text']").fadeToggle(1000);
 })
